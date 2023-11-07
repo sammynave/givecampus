930185c4426cb1dc3d882a82dc0369b328a3d0be
Sammy Nave <sammy.nave@gmail.com>
2023-11-07 14:24:18 -0500

    organize

---
+++ offline.rb
@@ -0,0 +1,52 @@
+class Offline
+  def initialize(rows:)
+    @rows = rows
+  end
+
+  def by_affiliation
+    affiliations = {}
+    @rows.each do |row|
+      donor = row['email'].downcase
+      affiliation = row['affiliation']
+      amount = row['amount'].to_f
+      next if affiliation.nil? || affiliation.size == 0 ||
+
+      if affiliations[affiliation]
+        affiliations[affiliation][:amount] += amount
+        affiliations[affiliation][:donations] += 1
+        affiliations[affiliation][:donors].add(donor)
+      else
+        affiliations[affiliation] = {
+          amount: amount,
+          donations: 1,
+          donors: Set.new([donor])
+        }
+      end
+
+    end
+    affiliations
+  end
+
+  def by_designation
+    designations = {}
+    @rows.each do |row|
+      donor = row['email'].downcase
+      designation_name = row['designation_name']
+      amount = row['designated_amount'].to_f
+      next if designation_name.nil? || designation_name.size == 0
+
+      if designations[designation_name]
+        designations[designation_name][:amount] += amount
+        designations[designation_name][:donations] += 1
+        designations[designation_name][:donors].add(donor)
+      else
+        designations[designation_name] = {
+          amount: amount,
+          donations: 1,
+          donors: Set.new([donor])
+        }
+      end
+    end
+    designations
+  end
+end
---
+++ online.rb
@@ -0,0 +1,50 @@
+class Online
+  def initialize(rows:)
+    @rows = rows
+  end
+
+  def by_designation
+    designations = {}
+    @rows.each do |row|
+      row_designations = JSON.parse(row['designation'])
+      donor = row['email'].downcase
+      row_designations.each do |(designation_name, amount)|
+        if designations[designation_name]
+          designations[designation_name][:amount] += amount.to_f
+          designations[designation_name][:donations] += 1
+          designations[designation_name][:donors].add(donor)
+        else
+          designations[designation_name] = {
+            amount: amount.to_f,
+            donations: 1,
+            donors: Set.new([donor])
+          }
+        end
+      end
+    end
+    designations
+  end
+
+  def by_affiliation
+    affiliations = {}
+    @rows.each do |row|
+      amount = row['amount'].to_f
+      donor = row['email'].downcase
+      row_affiliations = JSON.parse(row['affiliation'])
+      row_affiliations.keys.each do |affiliation|
+        if affiliations[affiliation]
+          affiliations[affiliation][:amount] += amount
+          affiliations[affiliation][:donations] += 1
+          affiliations[affiliation][:donors].add(donor)
+        else
+          affiliations[affiliation] = {
+            amount: amount,
+            donations: 1,
+            donors: Set.new([donor])
+          }
+        end
+      end
+    end
+    affiliations
+  end
+end
